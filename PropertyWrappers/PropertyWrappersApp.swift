fc6c108f0451df0428d131cbc8a1ae211145af82
Rafinad <rafinad@outlook.com>
2021-05-07 18:34:46 +0300

    Initial Commit

---
+++ PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Сахар Кубический on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
